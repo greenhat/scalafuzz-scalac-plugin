d64055ecad265376e06ba462a4780afea3151a6d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-10-14 18:51:19 +0300

    remove Corpus.needsReload;

--- scalafuzz-lib/src/main/scala/scalafuzz/internals/Corpus.scala
+++ scalafuzz-lib/src/main/scala/scalafuzz/internals/Corpus.scala
@@ -9,7 +9,6 @@ trait Corpus[F[_]] {
   def load: Seq[F[CorpusItem]]
   def add(input: CorpusItem): F[Unit]
   def added: Seq[F[CorpusItem]]
-  def needsReload: Boolean
 }
 
 object Corpus {
@@ -18,16 +17,8 @@ object Corpus {
 
 class IOCorpus extends Corpus[IO] {
 
-  private var _needsReload = true
-  override def needsReload: Boolean = _needsReload
-
   override def load: Seq[IO[Array[Byte]]] = {
-    _needsReload = false
-    Seq(
-      IO {
-        Array.emptyByteArray
-      }
-    )
+    Seq()
   }
 
   override def add(input: Array[Byte]): IO[Unit] = ???
