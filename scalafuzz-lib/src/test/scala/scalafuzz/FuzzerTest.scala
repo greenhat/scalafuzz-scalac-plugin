8935a499d41913389ed5e60c24e68ac6f57cb695
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-10-14 16:44:28 +0300

    extract "empty input on the first run" into the separate test;

--- scalafuzz-lib/src/test/scala/scalafuzz/FuzzerTest.scala
+++ scalafuzz-lib/src/test/scala/scalafuzz/FuzzerTest.scala
@@ -11,16 +11,11 @@ class FuzzerTest extends FunSuite
     val options = FuzzerOptions(
       TimedDuration(3.hours),
       exitOnFirstFailure = true)
-
     val expectedRunCountFail = 222
 
     object TargetObj {
       private var count: Int = 1
       def target(bytes: Array[Byte]): Unit = {
-        if (count == 1) {
-          // test empty input on first run
-          bytes.isEmpty shouldBe true
-        }
         if (count == expectedRunCountFail)
           throw new RuntimeException("catch me")
         else
@@ -32,6 +27,18 @@ class FuzzerTest extends FunSuite
     val report = Fuzzer.run(options, TargetObj.target)
     report.failures.length shouldBe 1
     report.stats.runCount shouldBe expectedRunCountFail
+  }
+
+  test("empty input on the first run") {
+    val options = FuzzerOptions(
+      TimedDuration(3.hours),
+      exitOnFirstFailure = true)
+    val report = Fuzzer.run(options, { bytes =>
+      bytes.isEmpty shouldBe true
+      throw new RuntimeException("catch me")
+    })
+    report.failures.length shouldBe 1
+    report.stats.runCount shouldBe 1
   }
 
 }
