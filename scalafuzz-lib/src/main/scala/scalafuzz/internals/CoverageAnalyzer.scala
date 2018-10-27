f0e8c60eb623483c3b4a0ab36d8f4707ba3f5f1f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-10-27 17:38:55 +0300

    draft CoverageAnalyzer imp;

--- scalafuzz-lib/src/main/scala/scalafuzz/internals/CoverageAnalyzer.scala
+++ scalafuzz-lib/src/main/scala/scalafuzz/internals/CoverageAnalyzer.scala
@@ -2,35 +2,22 @@ package scalafuzz.internals
 
 import java.util
 
-import cats.data.State
-import scalafuzz.internals.CoverageAnalyzer.{CoverageReport, NoNewCoverage}
+import scalafuzz.internals.CoverageAnalyzer.{CoverageReport, NewCoverage, NoNewCoverage}
+
+import scala.collection.mutable
 
 class CoverageAnalyzer {
 
-  private def update[S](f: S => S): State[S, Unit] = for {
-    v <- State.get[S]
-    nv = f(v)
-    _ <- State.set(nv)
-  } yield ()
+  private val store = mutable.Set[Int]()
 
-  protected def pushCoverageHash(hash: Int): State[List[Int], Unit] = update(hash :: _)
-  protected def hasCoverageHash(hash: Int): State[List[Int], Boolean] = State.get[List[Int]].map(_.contains(hash))
-
-  // todo: After each target run check if new coverage achieved
-  // (new features discovered, e.g. new invocations collected) then add the input to the corpus.
-  // todo: test
   def process(report: TargetRunReport): CoverageReport = {
     val hash = util.Arrays.hashCode(report.invocations.toArray)
-//    for {
-//      hasCoverage <- hasCoverageHash(hash)
-//      coverageReport <- if (!hasCoverage) {
-//        pushCoverageHash(hash)
-//        NewCoverage(report.input)
-//      } else
-//        NoNewCoverage
-//    } yield coverageReport
-    // todo fix
-    NoNewCoverage
+    if (store.contains(hash)) {
+       NoNewCoverage
+    } else {
+      store.add(hash)
+      NewCoverage(report.input)
+    }
   }
 }
 
--- scalafuzz-lib/src/test/scala/scalafuzz/internals/CoverageAnalyzerTest.scala
+++ scalafuzz-lib/src/test/scala/scalafuzz/internals/CoverageAnalyzerTest.scala
@@ -2,16 +2,45 @@ package scalafuzz.internals
 
 import org.scalatest.{FunSuite, Matchers}
 import scalafuzz.Invoker.InvocationId
-import scalafuzz.internals.CoverageAnalyzer.NoNewCoverage
+import scalafuzz.internals.CoverageAnalyzer.{NewCoverage, NoNewCoverage}
 
 class CoverageAnalyzerTest extends FunSuite with Matchers {
 
-  test("testProcess") {
-    val a = new CoverageAnalyzer
+  test("process - no stored coverages") {
+    val a = new CoverageAnalyzer()
+    val input1 = Array.fill[Byte](32)(1)
+    val coverage1 = Seq[InvocationId](1,2,3)
     a.process(
-      TargetRunReport(Array[Byte](), TargetNormalExit, Seq[InvocationId]())
-    ) shouldBe NoNewCoverage
-
+      TargetRunReport(input1, TargetNormalExit, coverage1)
+    ) shouldBe NewCoverage(input1)
   }
 
+  test("process - already in stored coverages") {
+    val a = new CoverageAnalyzer()
+    val input1 = Array.fill[Byte](32)(1)
+    val coverage1 = Seq[InvocationId](1,2,3)
+    a.process(
+      TargetRunReport(input1, TargetNormalExit, coverage1)
+    ) shouldBe NewCoverage(input1)
+
+    val input2 = Array.fill[Byte](32)(2)
+    a.process(
+      TargetRunReport(input2, TargetNormalExit, coverage1)
+    ) shouldBe NoNewCoverage
+  }
+
+  test("process - new coverage, not in stored coverages") {
+    val a = new CoverageAnalyzer()
+    val input1 = Array.fill[Byte](32)(1)
+    val coverage1 = Seq[InvocationId](1,2,3)
+    a.process(
+      TargetRunReport(input1, TargetNormalExit, coverage1)
+    ) shouldBe NewCoverage(input1)
+
+    val input2 = Array.fill[Byte](32)(2)
+    val coverage2 = Seq[InvocationId](1,2,3,4)
+    a.process(
+      TargetRunReport(input2, TargetNormalExit, coverage2)
+    ) shouldBe NewCoverage(input2)
+  }
 }
